827e53fe81c0fb6341eede18a7ac2ca34368a3fa
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-05 12:22:30 +0900

    서버 오류 처리하기

--- server/index.js
+++ server/index.js
@@ -5,6 +5,19 @@ app.get('/hello', (req, res) => {
   res.send('hello world !')
 })
 
+app.get('/error', (req, res) => {
+  throw new Error('서버에 치명적인 에러가 발생했습니다.')
+})
+
+// 폴백 핸들러 (fallback handler)
+app.use( (req, res, next) => {
+  res.status(404).send("Sorry can't find your page !")
+})
+app.use( (err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('something is broken on server !')
+})
+
 app.listen(5000, () => {
   console.log('server is running on port 5000...')
 })
