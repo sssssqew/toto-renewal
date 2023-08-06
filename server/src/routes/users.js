542411129d6a5c20382ef7d0c138429460c79f33
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-06 14:36:18 +0900

    회원가입 구현하기

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,20 +1,43 @@
 const express = require('express')
 const User = require('../models/User') 
+const expressAsyncHandler = require('express-async-handler') 
+const { generateToken, isAuth } = require('../../auth')
 
 const router = express.Router()
 
-router.post('/register', (req, res, next) => {
-  res.json("회원가입")
-})
+router.post('/register', expressAsyncHandler(async (req, res, next) => {
+  console.log(req.body)
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    userId: req.body.userId,
+    password: req.body.password,
+  })
+  const newUser = await user.save() // 사용자정보 DB 저장
+  if(!newUser){
+    res.status(401).json({ code: 401, message: 'Invalid User Data'})
+  }else{
+    const { name, email, userId, isAdmin, createdAt } = newUser 
+    res.json({ 
+      code: 200, 
+      token: generateToken(newUser), 
+      name, email, userId, isAdmin, createdAt
+    })
+  }
+}))
+
 router.post('/login', (req, res, next) => {
   res.json("로그인")
 })
+
 router.post('/logout', (req, res, next) => {
   res.json("로그아웃")
 })
+
 router.put('/:id', (req, res, next) => {
   res.json("사용자정보 변경")
 })
+
 router.delete('/:id', (req, res, next) => {
   res.json("사용자정보 삭제")
 })
