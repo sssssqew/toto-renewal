952cb9d39a0ca84f8a30b9bfcacf646bc6c91a06
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-06 15:01:11 +0900

    로그인 구현하기

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -26,9 +26,23 @@ router.post('/register', expressAsyncHandler(async (req, res, next) => {
   }
 }))
 
-router.post('/login', (req, res, next) => {
-  res.json("로그인")
-})
+router.post('/login', expressAsyncHandler(async (req, res, next) => {
+  console.log(req.body)
+  const loginUser = await User.findOne({
+    email: req.body.email, 
+    password: req.body.password,
+  })
+  if(!loginUser){
+    res.status(401).json({ code: 401, message: 'Invalid Email or Password' })
+  }else{
+    const { name, email, userId, isAdmin, createdAt } = loginUser 
+    res.json({ 
+      code: 200, 
+      token: generateToken(loginUser), 
+      name, email, userId, isAdmin, createdAt
+    })
+  }
+}))
 
 router.post('/logout', (req, res, next) => {
   res.json("로그아웃")
