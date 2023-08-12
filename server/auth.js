7a86912f6478cb7ba28da0045c681c268edbf1bf
sssssqew <sunrise.in.my.dream@gmail.com>
2023-08-12 22:14:41 +0900

    새로운 암호화 알고리즘 구현하기

--- server/auth.js
+++ server/auth.js
@@ -1,14 +1,98 @@
 const config = require('./config')
 const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
+const mongoose = require('mongoose')
+const { Types: { ObjectId } } = mongoose
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9]
+
+// 배열에서 랜덤한 값 선택
+const selectRandomValue = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+// 랜덤 문자열 생성
+const generateRandomString = n => {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  const str = new Array(n).fill('a')
+  return str.map(s => alphabet[Math.floor(Math.random()*alphabet.length)]).join("")
+}
+
+// 두 배열의 값을 믹스하고 문자열로 만들기
+const mixArraysToString = (arr1, arr2) => {
+  const length = arr1.length > arr2.length ?  arr1.length: arr2.length
+  let str = ''
+
+  for(let i=0; i<length; i++){
+    console.log(arr1[i], arr2[i])
+    str += arr1[i] || ''
+    str += '>'            // UUID 와 사용자정보를 구분하기 위한 구분문자
+    str += arr2[i] || ''
+    str += '>'
+  }
+  return str 
+}
+
+// 키로 설정한 배수의 인덱스 위치에 랜덤문자 추가하기
+const insertRandomStrToMixedUserInfo = (mixedUserInfo) => {
+  const pos = selectRandomValue(numbers) // 키 설정
+  return mixedUserInfo.map( (s, index) => index % pos === 0 ? generateRandomString(1) + s + ',' : s + ',').join('') + pos // 키의 값번째 배수에 랜덤문자 추가함
+}
+
+// 키로 설정한 배수의 인덱스 위치에서 랜덤문자 제거하기
+const removeRandomStrToEncryptedUserInfo = (encryptedUser) => {
+  const pos = encryptedUser[encryptedUser.length - 1] // 키 조회
+  console.log('키: ', pos)
+  encryptedUser = encryptedUser.slice(0, encryptedUser.length-1) // 키 제외
+  console.log(encryptedUser.split(',').map( (s, index) => index % pos === 0 ? s[1]: s).reverse().join(''))
+  return encryptedUser.split(',').map( (s, index) => index % pos === 0 ? s[1]: s) // 키의 값 번째 배수에서 랜덤문자 제거함
+}
+
+// 사용자정보 암호화하기
+const encrypt = (user) => {
+  // UUID 발급후 하이픈(-)으로 끊어서 사용자정보 문자열과 뒤섞기
+  const uuid = uuidv4().split('-')
+  console.log(uuid)
+
+  user = [
+    user._id.toString(), 
+    user.name, 
+    user.email, 
+    user.userId, 
+    user.isAdmin.toString(), 
+    user.createdAt.toString()
+  ]
+  
+  // 믹스된 사용자정보 문자열을 반대로 뒤짚기
+  const mixedUserInfo = mixArraysToString(user, uuid).split('').reverse()
+  console.log(mixedUserInfo)
+
+  // mixedUserInfo의 pos (2~9) 위치에 랜덤한 문자열을 추가하기
+  return insertRandomStrToMixedUserInfo(mixedUserInfo)
+}
+
+// 사용자정보 복호화하기
+const decrypt = (encryptedUser) => {
+  let decryptedUser = removeRandomStrToEncryptedUserInfo(encryptedUser)
+  .reverse().join('').split('>')
+  .filter( (field, index) => index % 2 === 0)
+  .filter( field => field !== '')
+
+  const [_id, name, email, userId, isAdmin, createdAt] = decryptedUser
+  
+  return {
+    _id: new ObjectId(_id),
+    createdAt: new Date(createdAt),
+    isAdmin: Boolean(isAdmin),
+    name, email, userId
+  }
+}
 
 const generateToken = (user) => { // 토큰 생성
+  const encryptedUser = encrypt(user)
+  console.log('encrypted user: ', encryptedUser)
+
   return jwt.sign({
-    _id: user._id,  // 사용자 정보 (json)
-    name: user.name,
-    email: user.email,
-    userId: user.userId,
-    isAdmin: user.isAdmin,
-    createdAt: user.createdAt,
+    encryptedUser   // 암호화된 사용자정보로부터 jwt 토큰 생성
   },
   config.JWT_SECRET, // jwt 비밀키
   {
@@ -29,7 +113,9 @@ const isAuth = (req, res, next) => { // 권한확인
       }else if(err){
         res.status(401).json({ code: 401, message: 'Invalid Token !'})
       }else{
-        req.user = userInfo
+        const decryptedUser = decrypt(userInfo.encryptedUser)
+        console.log("decrypted user: ", decryptedUser)
+        req.user = decryptedUser
         next()
       }
     })
